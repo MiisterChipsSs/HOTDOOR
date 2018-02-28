4f808a79106a22220a832bf1848909e851600d8e
Elouan <elouankleg@hotmail.fr>
2018-02-27 19:48:47 -0500

    First Commit. Unity Project with Player Movements scipt

---
+++ Hot door/Assets/Scripts/MovementsPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementsPlayer : MonoBehaviour
+{
+    [SerializeField]
+    private string VerticalAxis;
+    [SerializeField]
+    private string HorizontalAxis;
+    [SerializeField]
+    private float Speed = 0.5f;
+ 
+    //FixedUpdate se lance toutes les 0.02 secondes
+    void FixedUpdate ()
+	{  
+        //Permet de get les inputs 
+        float x = Input.GetAxisRaw(HorizontalAxis);
+	    float y = Input.GetAxisRaw(VerticalAxis);
+
+        //Crée le vecteur de déplacement
+        Vector2 direction = new Vector2(x * Speed, y * Speed);
+
+        //Applique le mouvement
+        transform.Translate(direction);
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Envoie un message dès qu'il détecte une collision avec qqch
+        Debug.Log("I hit : " + other.name );
+    }
+
+
+}
